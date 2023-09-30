54d2f734ef6592455bf1ae96a0e747b06981e060
finnac <finnac@mail.uc.edu>
2023-09-30 14:48:18 -0400

    Started work on reusing component

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,18 @@ function App() {
         title="Window Opening" 
         windowLabelText="% your window is open:" // Customizable label
       />
+      <ButtonOptions 
+        title="Bug Screen" 
+        windowLabelText="% your bug screen is on:" // Customizable label
+      />
+      <ButtonOptions 
+        title="Tint" 
+        windowLabelText="% your window is tinted:" // Customizable label
+      />
+       <ButtonOptions 
+        title="Privacy Blur" 
+        windowLabelText="% your window is blurred:" // Customizable label
+      />
     </div>
   );
 }
